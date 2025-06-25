a3b452d1be0aadd1ba96dfeadc226a71be353c1a
Roy <royelpemberton@gmail.com>
2025-06-25 18:10:21 -0400

    e

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,28 +1,18 @@
-// router.jsx
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "../resetStyle.css";
+import "../variables.css";
 
+import App from "../App/App.jsx";
 import Home from "../pages/home/home.jsx";
 import CountryDetail from "../components/country_detail.jsx";
 
-// Create a custom history with basename
-const customHistory = createBrowserHistory({ basename: "/REST-Countries-API" });
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/REST-Countries-API">
+    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/country/:name" element={<CountryDetail />} />
-    </>
-  ),
-  {
-    // 👇 Plug in custom history
-    history: customHistory,
-  }
+    </Routes>
+  </BrowserRouter>
 );
-
-export default router;
